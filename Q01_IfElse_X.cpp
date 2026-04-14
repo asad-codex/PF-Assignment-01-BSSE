f383a3382f8f13f417ee2582fbc6d4dfee650c70
asad-codex <asad.mail086@gmail.com>
2026-04-14 20:38:53 +0500

    Solved Question 01: If-else logic

---
+++ Q01_IfElse_X.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    int y;
+
+    cout<<"Enter the value of y: ";
+    cin>>y;
+
+    if (y == 10) {
+    x = 0;
+} else {
+    x = 1;
+}
+
+cout<<"The value of x is: "<<x <<endl;
+}
